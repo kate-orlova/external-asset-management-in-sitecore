0257eabebe6cb0d1570e8729c06ed9ba5b5f3db1
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-06 22:55:06 +0000

    Implemented ExternalService interface

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Interfaces/IExternalService.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Interfaces/IExternalService.cs
@@ -1,6 +1,9 @@
-﻿namespace Foundation.AssetManagement.Interfaces
+﻿using Foundation.AssetManagement.Models;
+
+namespace Foundation.AssetManagement.Interfaces
 {
     public interface IExternalService
     {
+         ExternalServiceSearchResponse Search(string query, int startFrom, int pageSize);
     }
 }
