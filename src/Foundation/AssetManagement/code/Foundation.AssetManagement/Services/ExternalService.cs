1e60202828b190a0778a0732bbb3662b50ce0c08
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-06 22:58:05 +0000

    Added an ExternalService class

---
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Services/ExternalService.cs
@@ -0,0 +1,14 @@
+﻿using Foundation.AssetManagement.Interfaces;
+using Foundation.AssetManagement.Models;
+using System;
+
+namespace Foundation.AssetManagement.Services
+{
+    public class ExternalService: IExternalService
+    {
+        public ExternalServiceSearchResponse Search(string query, int startFrom, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
