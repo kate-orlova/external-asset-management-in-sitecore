3d421ef6bfe1c8052f4d6902b6b141ebfece6e21
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-07 09:03:34 +0000

    Added a Json property "took" to ExternalServiceSearchResponse class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/ExternalServiceSearchResponse.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/ExternalServiceSearchResponse.cs
@@ -1,6 +1,9 @@
-﻿namespace Foundation.AssetManagement.Models
+﻿using Newtonsoft.Json;
+
+namespace Foundation.AssetManagement.Models
 {
     public class ExternalServiceSearchResponse
     {
+        [JsonProperty("took")] public int took { get; set; }
     }
 }
