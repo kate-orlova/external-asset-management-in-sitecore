56f79a4ad24dea6f3072947aafc27c3b4fc0f502
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-07 09:08:11 +0000

    Added a Json property "shards" to ExternalServiceSearchResponse class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/ExternalServiceSearchResponse.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/ExternalServiceSearchResponse.cs
@@ -6,5 +6,6 @@ namespace Foundation.AssetManagement.Models
     {
         [JsonProperty("took")] public int took { get; set; }
         [JsonProperty("timed_out")] public bool timed_out { get; set; }
+        [JsonProperty("shards")] public Shards _shards { get; set; }
     }
 }
