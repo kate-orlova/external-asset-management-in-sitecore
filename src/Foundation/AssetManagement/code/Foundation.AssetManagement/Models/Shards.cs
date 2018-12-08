387e020050074b950a1814dbf4a0fed27d567989
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-08 12:34:43 +0000

    Added a Successful property to Shards class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Shards.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Shards.cs
@@ -5,5 +5,6 @@ namespace Foundation.AssetManagement.Models
     public class Shards
     {
         [JsonProperty("total")] public int Total { get; set; }
+        [JsonProperty("successful")] public int Successful { get; set; }
     }
 }
