515b005ad4e780ba0ffbb5d687e502259272e98c
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-09 14:13:00 +0000

    Added a Json property "_index" to Hit class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Hit.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Hit.cs
@@ -1,6 +1,9 @@
-﻿namespace Foundation.AssetManagement.Models
+﻿using Newtonsoft.Json;
+
+namespace Foundation.AssetManagement.Models
 {
     public class Hit
     {
+        [JsonProperty("_index")] public string Index { get; set; }
     }
 }
