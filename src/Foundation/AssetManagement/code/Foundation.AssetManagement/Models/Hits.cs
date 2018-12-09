46559930c355971a818dff4fce82b7a1dca02ae8
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-09 14:09:19 +0000

    Added a MaxScore property to Hits class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Hits.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Hits.cs
@@ -5,5 +5,6 @@ namespace Foundation.AssetManagement.Models
     public class Hits
     {
         [JsonProperty("total")] public int Total { get; set; }
+        [JsonProperty("max_score")] public double? MaxScore { get; set; }
     }
 }
