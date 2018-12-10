1b81343cb0c55157ea2b908785d646172084a8e4
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-10 12:30:24 +0000

    Added a Json property Type to Hit class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Hit.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Hit.cs
@@ -5,5 +5,6 @@ namespace Foundation.AssetManagement.Models
     public class Hit
     {
         [JsonProperty("_index")] public string Index { get; set; }
+        [JsonProperty("_type")] public string Type { get; set; }
     }
 }
