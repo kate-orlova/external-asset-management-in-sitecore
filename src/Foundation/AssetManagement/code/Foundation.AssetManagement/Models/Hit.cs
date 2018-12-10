06f9a2312689385910951befa6440c973810d3d2
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-10 19:53:42 +0000

    Added a Json property Score to Hit class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Hit.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Hit.cs
@@ -7,5 +7,6 @@ namespace Foundation.AssetManagement.Models
         [JsonProperty("_index")] public string Index { get; set; }
         [JsonProperty("_type")] public string Type { get; set; }
         [JsonProperty("_id")] public string Id { get; set; }
+        [JsonProperty("_score")] public double Score { get; set; }
     }
 }
