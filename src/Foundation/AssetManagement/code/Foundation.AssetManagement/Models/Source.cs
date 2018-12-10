2df7bdf431fb7e60d833b00e5c0053610d5145a9
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-10 21:33:05 +0000

    Added a Json property Id to Source class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Source.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Source.cs
@@ -6,5 +6,6 @@ namespace Foundation.AssetManagement.Models
     {
         [JsonProperty("author")] public string Author { get; set; }
         [JsonProperty("country")] public string Country { get; set; }
+        [JsonProperty("id")] public long Id { get; set; }
     }
 }
