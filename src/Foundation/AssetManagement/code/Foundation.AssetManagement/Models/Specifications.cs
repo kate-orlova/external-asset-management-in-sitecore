5123058b676ec79d312161da4e9b76d2b23be0c2
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-11 09:10:46 +0000

    Added a Json property ColorSpace to Specifications class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Specifications.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Specifications.cs
@@ -1,6 +1,9 @@
-﻿namespace Foundation.AssetManagement.Models
+﻿using Newtonsoft.Json;
+
+namespace Foundation.AssetManagement.Models
 {
     public class Specifications
     {
+        [JsonProperty("File / Color space")] public string ColorSpace { get; set; }
     }
 }
