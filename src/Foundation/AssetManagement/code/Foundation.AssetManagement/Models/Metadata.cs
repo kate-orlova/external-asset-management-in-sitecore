11e091ef178260d64b0bee7b9fa336b7fdde92e1
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-12 18:12:35 +0000

    Added a Json property Specifications to Metadata class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Metadata.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Metadata.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Newtonsoft.Json;
 
 namespace Foundation.AssetManagement.Models
 {
     public class Metadata
     {
+        [JsonProperty("Specifications")] public Specifications Specifications { get; set; }
     }
 }
