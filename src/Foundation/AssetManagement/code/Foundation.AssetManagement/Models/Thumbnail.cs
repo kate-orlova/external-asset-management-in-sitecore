881a67318a6c529c46ea172c499490a98fda87c8
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-13 10:48:31 +0000

    Added a Json property MimeType to Thumbnail class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Thumbnail.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Thumbnail.cs
@@ -1,6 +1,9 @@
-﻿namespace Foundation.AssetManagement.Models
+﻿using Newtonsoft.Json;
+
+namespace Foundation.AssetManagement.Models
 {
     public class Thumbnail
     {
+        [JsonProperty("mime_type")] public string MimeType { get; set; }
     }
 }
