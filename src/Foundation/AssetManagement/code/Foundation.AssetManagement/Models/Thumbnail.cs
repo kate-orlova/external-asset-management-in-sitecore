f4ebad47496377dae4740f4dcc3bdb6c431c57c5
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-13 10:49:43 +0000

    Added a Json property Base64 to Thumbnail class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Thumbnail.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Models/Thumbnail.cs
@@ -5,5 +5,6 @@ namespace Foundation.AssetManagement.Models
     public class Thumbnail
     {
         [JsonProperty("mime_type")] public string MimeType { get; set; }
+        [JsonProperty("base64")] public string Base64 { get; set; }
     }
 }
