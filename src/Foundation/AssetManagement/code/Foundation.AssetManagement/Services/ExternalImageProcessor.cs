21d3571e3ac15c5d9416a0bd97c7e2d1d88f2545
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-15 14:12:44 +0000

    Inherited ExternalImageProcessor class from IExternalImageProcessor

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Services/ExternalImageProcessor.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Services/ExternalImageProcessor.cs
@@ -1,6 +1,17 @@
-﻿namespace Foundation.AssetManagement.Services
+﻿using Foundation.AssetManagement.Interfaces;
+
+namespace Foundation.AssetManagement.Services
 {
-    public class ExternalImageProcessor
+    public class ExternalImageProcessor : IExternalImageProcessor
     {
+        public string Resize(string url, int width, string additionalParameters)
+        {
+            return null;
+        }
+
+        public string Crop(string url, int width, int height)
+        {
+            return null;
+        }
     }
 }
