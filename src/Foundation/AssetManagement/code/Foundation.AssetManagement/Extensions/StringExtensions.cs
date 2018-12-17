2796d533d8a3dc9d5d904a0600442fccaf1ace55
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-17 10:35:49 +0000

    Implemented ToInt() extension method for string

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Extensions/StringExtensions.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Extensions/StringExtensions.cs
@@ -1,6 +1,15 @@
-﻿namespace Foundation.AssetManagement.Extensions
+﻿using Sitecore.StringExtensions;
+
+namespace Foundation.AssetManagement.Extensions
 {
-    public class StringExtensions
+    public static class StringExtensions
     {
+        public static int ToInt(this string target)
+        {
+            int result = 0;
+            if (!target.IsNullOrEmpty())
+                int.TryParse(target, out result);
+            return result;
+        }
     }
 }
