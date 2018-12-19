bd05e2e4d691427623bb8a3022532550a8a28538
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-19 22:12:40 +0000

    Added Crop() signature to IExternalImageProcessor interface

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Interfaces/IExternalImageProcessor.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Interfaces/IExternalImageProcessor.cs
@@ -3,5 +3,6 @@
     public interface IExternalImageProcessor
     {
         string Resize(string url, int width, string additionalParameters);
+        string Crop(string url, int width, int height);
     }
 }
