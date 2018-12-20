939eca950f09836609fa70db7512bb926293ccee
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-20 00:40:44 +0000

    Created GetRequestQueryString() in ExternalService class

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Services/ExternalService.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Services/ExternalService.cs
@@ -1,14 +1,31 @@
 ﻿using Foundation.AssetManagement.Interfaces;
 using Foundation.AssetManagement.Models;
 using System;
+using Newtonsoft.Json;
 
 namespace Foundation.AssetManagement.Services
 {
-    public class ExternalService: IExternalService
+    public class ExternalService : IExternalService
     {
         public ExternalServiceSearchResponse Search(string query, int startFrom, int pageSize)
         {
             throw new NotImplementedException();
+        }
+
+        private static string GetRequestQueryString(string query, int startFrom, int pageSize)
+        {
+            var requestObject = new
+            {
+                query = new {
+                    query = query
+
+                },
+                    from = startFrom,
+                    size = pageSize
+         
+            };
+            var requestString = JsonConvert.SerializeObject(requestObject);
+            return requestString;
         }
     }
 }
