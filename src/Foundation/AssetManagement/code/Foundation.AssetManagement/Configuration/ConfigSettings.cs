99d630b25adb92acadab871b68048bdf2b0b95c1
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-21 10:42:09 +0000

    Implemented GetSetting() in ConfigSettings

--- src/Foundation/AssetManagement/code/Foundation.AssetManagement/Configuration/ConfigSettings.cs
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Configuration/ConfigSettings.cs
@@ -2,6 +2,12 @@
 {
     public static class ConfigSettings
     {
-       
+
+        static string GetSetting(string settingName, string defaultValue = null)
+        {
+            var settingValue = Sitecore.Configuration.Settings.GetSetting(settingName);
+
+            return settingValue;
+        }
     }
 }
