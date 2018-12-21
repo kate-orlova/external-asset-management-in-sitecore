9f4ad43857ca30af7053d1b64629c3326deb7d8d
Kate Orlova <kate.orlova@deleteagency.com>
2018-12-21 10:31:57 +0000

    Added Constants class

---
+++ src/Foundation/AssetManagement/code/Foundation.AssetManagement/Configuration/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foundation.AssetManagement.Configuration
+{
+    public static class Constants
+    {
+        public static class AppSettings
+        {
+            public const string Host = "AssetManagement.Host";
+            public const string AccessKey = "AssetManagement.AccessKey";
+            public const string SecretKey = "AssetManagement.SecretKey";
+        }
+    }
+}
